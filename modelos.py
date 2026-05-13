1751761c6d2af5104e5584cb0e230caca9c5eb6a
Julean Pilligua <juleanpilligua2@gmail.com>
2026-05-13 09:11:54 -0500

    Creación de las 2 primeras clases

--- modelos.py
+++ modelos.py
@@ -1,1 +1,15 @@
 #Las clases Libros, Usuarios, Bibliotecario y Prestamos
+class Libro:
+    def __init__(self, idLibro, titulo, autor, genero, estado):
+        self.idLibro = idLibro
+        self.titulo = titulo
+        self.autor = autor
+        self.genero = []
+        self.estado = estado
+
+class Usuario:
+    def __init__(self, idUsuario, nombre, correo, tipo, libros_prestados):
+        self.__idUsuario = idUsuario
+        self.nombre = nombre
+        self._correo = correo
+        self._libros_prestados = []
