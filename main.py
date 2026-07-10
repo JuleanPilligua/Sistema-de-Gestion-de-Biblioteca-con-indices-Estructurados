f00fa6e5b15a44243e3dc65161b3ce6fcce5038d
Pilligua Saltos Michael Julean <juleanpilligua2@gmail.com>
2026-07-09 21:45:28 -0500

    Retoques finales

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ def inicializar_datos(biblioteca):
     biblioteca.registrar_usuario("L01", "Admin Biblioteca", "admin@biblioteca.com", "admin", "Bibliotecario", "EMP101")
     
     # Registrar Clientes por defecto
-    biblioteca.registrar_usuario("C01", "Juan Pérez", "juan@correo.com", "123", "Cliente")
-    biblioteca.registrar_usuario("C02", "María López", "maria@correo.com", "123", "Cliente")
-    biblioteca.registrar_usuario("C03", "Carlos Mendoza", "carlos@correo.com", "123", "Cliente")
+    # biblioteca.registrar_usuario("C01", "Juan Pérez", "juan@correo.com", "123", "Cliente")
+    # biblioteca.registrar_usuario("C02", "María López", "maria@correo.com", "123", "Cliente")
+    # biblioteca.registrar_usuario("C03", "Carlos Mendoza", "carlos@correo.com", "123", "Cliente")
     
     # Registrar Libros por defecto
-    biblioteca.registrar_libro("9781", "Cien años de soledad", "Gabriel García Márquez")
-    biblioteca.registrar_libro("9782", "Don Quijote de la Mancha", "Miguel de Cervantes")
-    biblioteca.registrar_libro("9783", "El Principito", "Antoine de Saint-Exupéry")
-    biblioteca.registrar_libro("9784", "Ficciones", "Jorge Luis Borges")
+    # biblioteca.registrar_libro("9781", "Cien años de soledad", "Gabriel García Márquez")
+    # biblioteca.registrar_libro("9782", "Don Quijote de la Mancha", "Miguel de Cervantes")
+    # biblioteca.registrar_libro("9783", "El Principito", "Antoine de Saint-Exupéry")
+    # biblioteca.registrar_libro("9784", "Ficciones", "Jorge Luis Borges")
     
     # Vaciar el historial inicial para que las acciones de inicialización no llenen la pila del usuario
     biblioteca.historial.vaciar()
